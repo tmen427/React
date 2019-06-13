3c39f33382a90b644a7749cac43e6a5cfd466d1a
tony <tmonnie@tonys-MacBook-Air.local>
2019-06-13 15:05:23 -0500

    Updates

---
+++ precache-manifest.91a2b26da1af0d9c66925050eef4377c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9259ce9f8fda6fe9bdcfad99d4908068",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccbca7f794f98003704f",
+    "url": "/static/css/main.7e2f8446.chunk.css"
+  },
+  {
+    "revision": "6abbc8480ecc1b7feda2",
+    "url": "/static/js/2.06a9d899.chunk.js"
+  },
+  {
+    "revision": "ccbca7f794f98003704f",
+    "url": "/static/js/main.c36b6086.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
